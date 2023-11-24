1b5851a364b128d8c7436f596c4f951946c55117
Mehedi Mohammad Himel <mehedihimel99@gmail.com>
2023-11-24 22:17:36 +0600

    updated the train data added 3 option of [greeting,name,capability]

--- chat.py
+++ chat.py
@@ -25,7 +25,7 @@ model = NeuralNet(input_size, hidden_size, output_size).to(device)
 model.load_state_dict(model_state)
 model.eval()
 
-bot_name = "ChatBot"
+bot_name = "Settler"
 while True:
     # sentence = "do you use credit cards?"
     sentence = input("You: ")
